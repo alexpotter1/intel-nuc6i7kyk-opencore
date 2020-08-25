0b07b8ff83a8dbfc6e470c0f42266e77c0c0a11d
Alex Potter <alexpotter1@icloud.com>
2020-08-25 20:49:24 +0100

    Download itlwm/Black80211 from usr-sse2's repo
    https://github.com/usr-sse2/Black80211-Catalina
    Allows native use of Apple WiFi menu for controlling itlwm

--- get-kexts.sh
+++ get-kexts.sh
@@ -17,8 +17,8 @@ read -p 'Do you want to enable Intel WiFi support? (experimental) [y/N]: ' wifi_
 # lowercase
 wifi_input=$(echo "$wifi_input" | tr '[:upper:]' '[:lower:]')
 if [[ $wifi_input == 'y' ]]; then
-    echo 'Enabled Intel WiFi support'
-    echo 'This kext is not considered production-ready yet. Use at own risk.'
+    echo 'Enabled Intel WiFi support (with Black80211)'
+    echo 'These kexts are not considered production-ready yet. Use at own risk.'
     echo ""
     wifi_enabled=1
     sleep 1
@@ -27,13 +27,14 @@ fi
 if [[ $wifi_enabled -eq 1 ]]; then
     # remove newline, add new info and re-append newline
     perl -pi -e 'chomp if eof' kexts.txt
-    echo -e "\nOpenIntelWireless-Factory	1hbb	0" >> kexts.txt
+    echo -e "\nBlack80211-Catalina	usr-sse2	0" >> kexts.txt
 fi
 
 itlwm_enabled_already=$(/usr/libexec/PlistBuddy -c "Print :Kernel:Add:9:Enabled" EFI/OC/config.plist)
 if [ "$wifi_enabled" -eq 0 ] && [[ "$itlwm_enabled_already" == "true" ]]; then
     echo "Resetting itlwm enabled state in config.plist"
     /usr/libexec/PlistBuddy -c "Set :Kernel:Add:9:Enabled false" EFI/OC/config.plist
+    /usr/libexec/PlistBuddy -c "Set :Kernel:Add:10:Enabled false" EFI/OC/config.plist
 fi
 
 while IFS=$'\t' read -r -a kext_lines
@@ -93,29 +94,26 @@ find . -name "IntelBluetoothInjector.kext" -type d -exec rm -r {} +
 find . -name "XHCI-unsupported.kext" -type d -exec rm -r {} +
 if [[ $wifi_enabled -eq 1 ]]; then
     find . -name "itlwmx.kext" -type d -exec rm -r {} +
+    find . -name "Catalina" -type d -exec rm -r {} +
 fi
 
 find . -name "*.kext" -exec cp -r {} ../../EFI/OC/Kexts/ \;
 
 if [[ $wifi_enabled -eq 1 ]]; then
-    find . -name "HeliPort.app" -exec cp -r {} ../../EFI/OC/ \;
     echo ""
-    echo "The Intel WiFi driver (itlwm.kext) has been moved to EFI/OC/Kexts."
+    echo "The Intel WiFi driver (itlwm.kext) is now controllable with the default Apple WiFi interface."
     sleep 2
-    echo "The controlling app, HeliPort, has been moved to EFI/OC."
+    echo "HeliPort is no longer required."
     sleep 2
-    echo "After installation, mount your EFI and copy the HeliPort app to /Applications."
+    echo "You may find that Location Services now works with no additional configuration."
     sleep 2
-    echo "From there, it will provide control for connecting to wireless networks."
-    sleep 2
-    echo "You can also tell it to launch at login."
-    sleep 2
-    echo "For more help, go to https://github.com/OpenIntelWireless."
+    echo "For more help, go to https://github.com/usr-sse2/Black80211-Catalina."
     sleep 2
     echo ""
 
     echo "Patching config.plist"
     /usr/libexec/PlistBuddy -c "Set :Kernel:Add:9:Enabled true" ../../EFI/OC/config.plist
+    /usr/libexec/PlistBuddy -c "Set :Kernel:Add:10:Enabled true" ../../EFI/OC/config.plist
     sed -i '' -e '$ d' ../../kexts.txt
 fi
 
